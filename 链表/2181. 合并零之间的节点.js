21a641e612c9ef351e85ace48c68a29f08aa5bb5
hanhaonan1111 <hhnboy@163.com>
2023-01-02 10:52:02 +0800

    +1

---
+++ 链表/2181. 合并零之间的节点.js
@@ -0,0 +1,72 @@
+/**给你一个链表的头节点 head ，该链表包含由 0 分隔开的一连串整数。
+ * 链表的 开端 和 末尾 的节点都满足 Node.val == 0 。
+对于每两个相邻的 0 ，请你将它们之间的所有节点合并成一个节点，
+其值是所有已合并节点的值之和。然后将所有 0 移除，修改后的链表不应该含有任何 0 。
+ 返回修改后链表的头节点 head 。 */
+
+function ListNode(val, next) {
+    this.val = (val === undefined ? 0 : val)
+    this.next = (next === undefined ? null : next)
+}
+// 正式代码
+// 法1 :模拟
+let mergeNodes1 = function (head) {
+    let total = 0
+    let link = head
+    let cur = head
+    while (link) {
+        if (link.val === 0) {
+            if (total !== 0) {
+                cur.next = new ListNode(total, link)
+                cur = link  // 当前0节点
+                total = 0   // 初始化总数
+            }
+            else {
+                cur = link  // 当前0节点
+                total = 0   // 初始化总数
+            }
+        }
+        total += link.val
+        link = link.next
+    }
+    link = head
+    while (link) {
+        if (link.next.val === 0) {
+            link.next = link.next.next ? link.next.next : null
+        }
+        link = link.next
+    }
+    return head.next ? head.next : null
+};
+// 法2:(模拟)创新版
+let mergeNodes = function (head) {
+    let total = 0
+    let tail = new ListNode(-1)
+    let tailNode = tail
+    while (head) {
+        if (head.val === 0) {
+            if (total !== 0) {
+                tailNode.next = new ListNode(total, null)
+                total = 0
+                tailNode = tailNode.next
+            }
+        }
+        head = head.next
+        total = total + head?.val
+    }
+    return tail.next
+};
+// 测试数据
+let node = new ListNode(0, new ListNode(1, new ListNode(0, new ListNode(9, new ListNode(
+    0, new ListNode(-4, new ListNode(0))
+)))))
+
+let res = mergeNodes(node)
+console.log(res, 'ress');
+
+
+
+
+
+
+
