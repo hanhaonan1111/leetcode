740c96f4e8dd5e84d8c95ea852970de9ad65fd50
hanhaonan1111 <hhnboy@163.com>
2023-01-05 14:33:11 +0800

    +1

--- 哈希表/1. 两数之和.js
+++ 哈希表/1. 两数之和.js
@@ -4,6 +4,30 @@
  * 数组中同一个元素在答案里不能重复出现。你可以按任意顺序返回答案。
  */
 
-var twoSum = function (nums, target) {
 
+// 法1:暴力破解法
+var twoSum1 = function (nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        let cur = nums[i]
+        for (let j = i + 1; j < nums.length; j++) {
+            let other = nums[j]
+            if (cur + other === target) {
+                return [i, j]
+            }
+        }
+    }
 };
+// 法2
+// var twoSum = function (nums, target) {
+//     let map = new Map();
+//     for (var i = 0; i < nums.length - 1; i++) {
+//         if (map.has(nums[i])) {
+//             return [map.get(nums[i]), i];
+//         } else {
+//             map.set(target - nums[i], i)
+//         }
+//     }
+// }
+
+let res = twoSum([3, 3], 6)
+console.log(res, 'resUlt');
