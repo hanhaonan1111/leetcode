1e31913b913044b66cfe1f883f69d28c7a7d054f
hanhaonan1111 <hhnboy@163.com>
2023-01-05 10:30:13 +0800

    +1

---
+++ 哈希表/349. 两个数组的交集.js
@@ -0,0 +1,26 @@
+/*给定两个数组 nums1 和 nums2 ，返回 它们的交集 。输出结果中的每个元
+素一定是 唯一 的。我们可以 不考虑输出结果的顺序 。*/
+
+
+var intersection = function (num1, num2) {
+    console.log(num1, num2);
+
+
+
+
+};
+
+
+intersection([1, 2, 2, 1], [2, 2])
+
+
+
+
+
+
+
+
+
+
+
+
