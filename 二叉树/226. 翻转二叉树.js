388143df050e8315190812c5c5021c15c5d68ceb
hanhaonan1111 <hhnboy@163.com>
2023-01-18 10:32:52 +0800

    +1

---
+++ 二叉树/226. 翻转二叉树.js
@@ -0,0 +1,34 @@
+/**给你一棵二叉树的根节点 root ，翻转这棵二叉树，并返回其根节点。
+ */
+function TreeNode(val, left, right) {
+    this.val = (val === undefined ? 0 : val)
+    this.left = (left === undefined ? null : left)
+    this.right = (right === undefined ? null : right)
+}
+
+//正式数据
+var invertTree = function (node) {
+    function Recursion(node) {
+        if (node.left || node.right) {
+            // 两个都节点不存在  回朔 
+            let other
+            other = node.left
+            node.left = node.right
+            node.right = other
+            node.left && Recursion(node.left)
+            node.right && Recursion(node.right)
+        }
+    }
+    if (!node) {
+        return node
+    } else {
+        Recursion(node)
+        return node
+    }
+};
+// 测试数据
+let root = new TreeNode(1,
+    new TreeNode(0, new TreeNode(5, new TreeNode(7), new TreeNode(12)), new TreeNode(8)),
+    new TreeNode(-9))
+let res = invertTree(root)
+console.log(res)
