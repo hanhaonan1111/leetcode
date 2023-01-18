58e5de02514694cedfc125ae2a6e97c0a949174e
hanhaonan1111 <hhnboy@163.com>
2023-01-18 10:15:59 +0800

    +1

--- 二叉树/102. 二叉树的层序遍历.js
+++ 二叉树/102. 二叉树的层序遍历.js
@@ -10,9 +10,30 @@ function TreeNode(val, left, right) {
 
 //正式数据
 var levelOrder = function (root) {
-
+    let queue = []
+    let res = []
+    root && queue.push(root)
+    while (queue.length > 0) {
+        let continer = []
+        let len = queue.length
+        //循环的终止条件不能不生是queue.length;必须是一个准确的数字.想想为什么?
+        for (let i = 0; i < len; i++) {
+            let head = queue.shift()
+            continer.push(head.val)
+            // 因为[]是复杂数据类型,这里会直接影响循环终止的条件
+            if (head.left) queue.push(head.left)
+            if (head.right) queue.push(head.right)
+        }
+        res.push([...continer])
+    }
+    console.log(res, 'RES');
+    return res
 };
 // 测试数据
-let root = new TreeNode(1, new TreeNode(0, new TreeNode(5, new TreeNode(7), new TreeNode(12)), new TreeNode(8)), new TreeNode(-9))
+let root = new TreeNode(1,
+    new TreeNode(0, new TreeNode(5, new TreeNode(7), new TreeNode(12)),
+        new TreeNode(8)), new TreeNode(-9))
+console.log(root);
 let res = levelOrder(root)
+
 console.log(res)
