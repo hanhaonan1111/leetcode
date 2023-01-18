05c64c003f77a16dcf4ac47e0c35b1caffdc7961
hanhaonan1111 <hhnboy@163.com>
2023-01-18 15:06:00 +0800

    +1

--- 二叉树/101. 对称二叉树.js
+++ 二叉树/101. 对称二叉树.js
@@ -7,7 +7,8 @@ function TreeNode(val, left, right) {
 }
 
 //正式代码
-var isSymmetric = function (root) {
+// 法1.深度优先
+var isSymmetric1 = function (root) {
     if (!root) return true
     function Digui(left, right) {
         //详尽描述3种情况
@@ -15,19 +16,36 @@ var isSymmetric = function (root) {
         if ((!left && right) || (left && !right)) {
             return false
         }
-        if (left.val != right.val) {
+        // 这里一定要判断不符合条件的情况
+        if (left.val !== right.val) {
             return false
         }
+        // 能走到这里,说明目前为止,所遍历的节点均对称!
         return Digui(left.left, right.right) && Digui(left.right, right.left)
     }
-
-
     let res = Digui(root.left, root.right)
-    console.log(res, 'RES');
     return res
 }
 
-
+// 法2:广度优先
+var isSymmetric = function (root) {
+    if (!root) return true
+    let queue = [root.left, root.right]
+    while (queue.length) {
+        let len = queue.length
+        for (let i = 0; i < len; i += 2) {
+            let l = queue.shift()
+            let r = queue.shift()
+            if ((!l && r) || (!r && l)) return false
+            if (l && r) {
+                if (l.val !== r.val) return false
+                queue.push(l.left, r.right)
+                queue.push(l.right, r.left)
+            }
+        }
+    }
+    return true
+}
 
 // 测试数据
 let root = new TreeNode(1,
---
+++ 二叉树/104. 二叉树的最大深度.js
@@ -0,0 +1,50 @@
+
+/**
+ * 给定一个二叉树，找出其最大深度。
+二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+说明: 叶子节点是指没有子节点的节点。
+ */
+
+function TreeNode(val, left, right) {
+    this.val = (val === undefined ? 0 : val)
+    this.left = (left === undefined ? null : left)
+    this.right = (right === undefined ? null : right)
+}
+
+//正式代码
+// 法1:循环
+var maxDepth1 = function (root) {
+    if (!root) return 0
+    let queue = [root]
+    let count = 0
+    while (queue.length) {
+        let len = queue.length
+        for (let i = 0; i < len; i++) {
+            let head = queue.shift()
+            head.left && queue.push(head.left)
+            head.right && queue.push(head.right)
+        }
+        count++
+    }
+    return count
+};
+
+// 法2:递归
+function maxDepth(root) {
+    if (!root) return 0
+    function Digui(node) {
+        if (node == null) {
+            return 0
+        }
+        let lf = Digui(node.left)
+        let ri = Digui(node.right)
+        return Math.max(lf, ri) + 1
+    }
+    Digui(root)
+}
+// 测试数据
+let root = new TreeNode(1,
+    new TreeNode(0, new TreeNode(5, new TreeNode(7), new TreeNode(12)), new TreeNode(8)),
+    new TreeNode(-9))
+let res = maxDepth(root)
+console.log(res)
