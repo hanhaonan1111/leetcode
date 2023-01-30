9dc0d29a5cfa03a0485ec0a178728b16b07c3a43
hanhaonan1111 <hhnboy@163.com>
2023-01-30 21:43:10 +0800

    回溯算法+1

--- 二叉树/450. 删除二叉搜索树中的节点.js
+++ 二叉树/450. 删除二叉搜索树中的节点.js
@@ -11,41 +11,87 @@ function TreeNode(val, left, right) {
     this.right = (right === undefined ? null : right)
 }
 //正式代码
-var deleteNode = function (root, val) {
-    let del = null
-
-    function address(node) {
-        if (node.left) {
-            return address(node.left)
-        } else {
+// 
+var deleteNode1 = function (root, target) {
+    function DG(node) {
+        if (!node) return null
+        // 遍历右边
+        if (node.val < target) {
+            node.right = DG(node.right)
             return node
         }
-
-    }
-    function DG(node) {
-        if (node.val < val) {
-            DG(node.right)
-        } else if (node.val > val) {
-            DG(node.left)
-        } else {
-            if (!node) return root
-            // 找到了目标值
-            if (node.right) {
-                del = node
-                let target = address(node.right)
-                node.val = target.val
+        // 遍历左边
+        if (node.val > target) {
+            node.left = DG(node.left)
+            return node
+        }
+        if (node.val === target) {
+            if (!node.left) {
+                // 没有左边
+                return node.right
             }
-
-
+            else if (!node.right) {
+                //没有右边
+                return node.left
+            }
+            else if (!node.right && !node.left) {
+                // 叶子节点
+                return null
+            } else {
+                // 子节点
+                let cur = node.right
+                while (cur.left) {
+                    cur = cur.left
+                }
+                console.log(node, cur, '{}');
+                cur.left = node.left
+                node = node.right
+                return node
+            }
         }
     }
+    return DG(root)
 
-    DG(root)
-    console.log('RES', root);
 };
+//理清思路
+function deleteNode(node, key) {
+    function DG(node) {
+        if (!node) return null
+
+        if (node.val > key) {
+            node.left = DG(node.left)
+            return node
+        }
+        if (node.val < key) {
+            node.right = DG(node.right)
+            return node
+        }
+        if (node.val === key) {
+            if (!node.left) {
+                return node.right
+            } else if (!node.right) {
+                return node.left
+            } else if (!node.left && !node.right) {
+                return null
+            } else {
+                // node 是被删节点
+                let cur = node.right
+                while (cur.left) {
+                    cur = cur.left
+                }
+                // cur是替代节点
+                cur.left = node.left
+                node = node.right
+                return node
+            }
+        }
+    }
+    return DG(node)
+}
+
 //测试数据
 let root = new TreeNode(5,
     new TreeNode(3, new TreeNode(2), new TreeNode(4)),
     new TreeNode(6, null, new TreeNode(7)))
-let res = deleteNode(root, 3)
-console.log(res);       
+let res = deleteNode(root, 4)
+console.log(res, 'res');       
